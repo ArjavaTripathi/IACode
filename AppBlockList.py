5c666c1a6613ec28ec2c771f67f385f4f177b11f
ArjavaTripathi <100183634+ArjavaTripathi@users.noreply.github.com>
2022-11-03 16:58:14 +0530

    updates

--- AppBlockList.py
+++ AppBlockList.py
@@ -1,42 +1,51 @@
 import subprocess
-from DatabaseStorage import *
+from DatabaseStorage import DatabaseStorageClass
 from datetime import date
 import re
 
-ActiveProcesses = []
-StoreProcesses = []
 
-totalamount = 0
+class BlockList(DatabaseStorageClass):
+    def __init__(self):
+        self.ActiveProcesses = []
+        self.totalamount = 0
+        self.getProcess()
+
+    def getProcess(self):  # Getting open processes
+        cmd = 'powershell "gps | where {$_.MainWindowTitle } | select Description'
+        proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        for line in proc.stdout:
+            if line.rstrip():
+                # only print lines that are not empty
+                # decode() is necessary to get rid of the binary string (b')
+                # rstrip() to remove `\r\n`
+                print(line.decode().rstrip())
+                self.ActiveProcesses.append(line.decode().rstrip())
+                self.editArray()
+
+    def editArray(self):
+
+        del self.ActiveProcesses[0:3]
+
+        for a in self.ActiveProcesses:  # Counts How Many Active Processes
+            self.totalamount += 1
+
+            today = str(date.today())  # Reformats Date
+            retoday = re.sub("-", "/", today)
+            self.ActiveProcesses.append(retoday)
+
+            self.StoreIntoDB()
+
+        print(self.ActiveProcesses)
+
+    def StoreIntoDB(self):
+
+        for a in self.ActiveProesses:  # Stores into database
+            currentamount = 0
+            while (currentamount < self.totalamount):
+                param = (a, self.ActiveProesses[-1])
+                DatabaseStorageClass().InsertIntoDatabase()
+
+            currentamount += 1
 
 
-def getProcess():  # Getting open processes
-    cmd = 'powershell "gps | where {$_.MainWindowTitle } | select Description'
-    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-    for line in proc.stdout:
-        if line.rstrip():
-            # only print lines that are not empty
-            # decode() is necessary to get rid of the binary string (b')
-            # rstrip() to remove `\r\n`
-            print(line.decode().rstrip())
-            ActiveProcesses.append(line.decode().rstrip())
-
-
-getProcess()
-
-del ActiveProcesses[0:3]
-
-
-for a in ActiveProcesses:  # Adds to StoreProcesses
-    StoreProcesses.append(a)
-    totalamount += 1
-
-today = str(date.today())  # Reformats Date
-retoday = re.sub("-", "/", today)
-StoreProcesses.append(retoday)
-
-
-for a in StoreProcesses:  # Stores into database
-    currentamount = 0
-    while (currentamount < totalamount):
-        param = (a, StoreProcesses[-1])
-        cursor.execute(InsertInit, param)
+BlockList()
