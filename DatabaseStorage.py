98308b4cd7eb7a582c7fe98ed738195037c3fb18
ArjavaTripathi <100183634+ArjavaTripathi@users.noreply.github.com>
2022-11-03 16:36:24 +0530

    Created DATABASE CLASS and LOGINGUI Almost Done

--- DatabaseStorage.py
+++ DatabaseStorage.py
@@ -1,12 +1,24 @@
 import sqlite3
 
-conn = sqlite3.connect('processes.db')
-cursor = conn.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS StoringData(
-                APP TEXT not null,
-                LABELS TEXT,
-                IGNORED BIT NOT NULL,
-                DATE_OPEN TEXT
-            )''')
 
-InsertInit = '''INSERT INTO StoringData(APP, DATE_OPEN) VALUES(?,?)'''
+class DatabaseStorageClass:
+    def __init__(self, ):
+        self.conn = sqlite3.connect('processes.db')
+        self.cursor = self.conn.cursor()
+
+    def makeDatabase(self):
+
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS StoringData(
+                        APP TEXT not null,
+                        LABELS TEXT,
+                        IGNORED BIT NOT NULL,
+                        DATE_OPEN TEXT
+                    )''')
+
+    def InsertIntoDatabase(self, APP, LABELS, IGNORED, DATE_OPEN):
+
+        InsertInit = '''INSERT INTO StoringData(APP, DATE_OPEN) VALUES(?,?)'''
+        params = (APP, LABELS, IGNORED, DATE_OPEN)
+
+        self.cursor.execute(InsertInit, params)
+        self.conn.commit()
